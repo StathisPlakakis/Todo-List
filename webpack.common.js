cd31fb0d4cfb82d9e0ed1cf23f8953c8f3e8075c
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-25 18:51:53 +0300

    Update common config to use  html template

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 
 module.exports = {
@@ -8,5 +10,12 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-      },
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Output Management',
+        inject: 'body',
+    }),
+    ],
     };
