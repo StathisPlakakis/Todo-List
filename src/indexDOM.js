228295e42458963fcf0136a7d033f936f40b1601
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-28 18:17:31 +0300

    Forbid submition of blank project title

--- src/indexDOM.js
+++ src/indexDOM.js
@@ -21,19 +21,21 @@ projectInput.addEventListener("input", (e) => {
 
 const submit = document.querySelector(".submit");
 submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    const newProject = new Project(projectInput.value);
-    const allProjects = document.querySelector(".project-list");
-    allProjects.innerHTML = "";
-    Project.myProjects.forEach((project, index) => {
-        const projectElement = document.createElement("div");
-        projectElement.textContent = project.title;
-        projectElement.setAttribute("index", index);
-        projectElement.classList.add("project-sidebar");
-        allProjects.appendChild(projectElement);
-    })
-    maxLength.textContent = "0 / 50";
-    projectInput.value = "";
-    dialogProjectTitlte.close();
+    if (projectInput.value.length > 0) {
+        e.preventDefault();
+        const newProject = new Project(projectInput.value);
+        const allProjects = document.querySelector(".project-list");
+        allProjects.innerHTML = "";
+        Project.myProjects.forEach((project, index) => {
+            const projectElement = document.createElement("div");
+            projectElement.textContent = project.title;
+            projectElement.setAttribute("index", index);
+            projectElement.classList.add("project-sidebar");
+            allProjects.appendChild(projectElement);
+        })
+        maxLength.textContent = "0 / 50";
+        projectInput.value = "";
+        dialogProjectTitlte.close();
+    }
 })
 
