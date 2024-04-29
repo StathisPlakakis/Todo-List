a9e67daa0b519c94640669ac28c514ca9d44e7c5
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-29 20:46:33 +0300

    Add and style the button for task creation

--- src/indexProjectInfo.js
+++ src/indexProjectInfo.js
@@ -5,7 +5,12 @@ class ProjectInfo {
         const title = document.createElement("div");
         title.classList.add("projectInfoTitle");
         title.textContent = project.title;
-        projectViewport.appendChild(title)
+        projectViewport.appendChild(title);
+        const addNewTask = document.createElement("button");
+        addNewTask.textContent = "Add new Task";
+        addNewTask.classList.add("create-task")
+        projectViewport.appendChild(addNewTask);
+
     }
 }
 
