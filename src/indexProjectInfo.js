7345db4a940a4737b84eb9b8cba0bc9e6e57d7fd
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-29 23:37:17 +0300

    Add the dialog for task creatment

--- src/indexProjectInfo.js
+++ src/indexProjectInfo.js
@@ -1,14 +1,19 @@
 class ProjectInfo {
     static projectInfoRender (project) {
+        const taskDialog = document.querySelector(".newTaskDialog");
         const projectViewport = document.querySelector(".project");
         projectViewport.innerHTML = "";
+        projectViewport.appendChild(taskDialog);
         const title = document.createElement("div");
         title.classList.add("projectInfoTitle");
         title.textContent = project.title;
         projectViewport.appendChild(title);
         const addNewTask = document.createElement("button");
         addNewTask.textContent = "Add new Task";
-        addNewTask.classList.add("create-task")
+        addNewTask.classList.add("create-task");
+        addNewTask.addEventListener("click", () => {
+            taskDialog.showModal();
+        })
         projectViewport.appendChild(addNewTask);
 
     }
